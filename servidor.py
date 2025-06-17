14161cda204fd0f602fe149671e1691313e13ce7
DavidS3gura <136142191+DavidS3gura@users.noreply.github.com>
2025-06-16 23:14:25 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 HUGGINGFACE_API_TOKEN = os.environ.get("HUGGINGFACE_API_TOKEN")
-HUGGINGFACE_MODEL = "mistralai/Mistral-7B-Instruct-v0.1"
+HUGGINGFACE_MODEL = "google/flan-t5-large"
 
 headers = {
     "Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}",
